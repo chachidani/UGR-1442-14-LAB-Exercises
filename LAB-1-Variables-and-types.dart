390b0db3b027c3a8fbacf3a7161558aba7fb6948
chachidani <141312783+chachidani@users.noreply.github.com>
2024-03-08 05:53:09 -0900

    variables and types

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  String fullName = "Nardos";
+  int myAge = 30;
+  String bestColor = "blue";
+
+  print("Name: $fullName");
+  print("Age: $myAge");
+  print("Favorite Color: $bestColor");
+}
---
+++ LAB-2-Variables-and-types.dart
@@ -0,0 +1,5 @@
+void main() {
+  String myString = "Hello, World!";
+  int stringLength = myString.length;
+  print("The length of the string is: $stringLength");
+}
